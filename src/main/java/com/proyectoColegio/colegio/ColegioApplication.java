30b2241861ada4d8a93888dab07aa342da1ab62f
Kevin Calzada <giancarlocalzada@gmail.com>
2022-08-30 18:53:56 -0500

    hello

--- src/main/java/com/proyectoColegio/colegio/ColegioApplication.java
+++ src/main/java/com/proyectoColegio/colegio/ColegioApplication.java
@@ -2,9 +2,19 @@ package com.proyectoColegio.colegio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ColegioApplication {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "hola negros";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ColegioApplication.class, args);
