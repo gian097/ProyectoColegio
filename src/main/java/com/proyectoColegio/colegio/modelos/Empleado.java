f4f52db092fc359a8a3a35db7ca6fa959c531667
Kevin Calzada <giancarlocalzada@gmail.com>
2022-08-30 19:49:49 -0500

    creacion de la tres clases colegios, empleado, moviemnto de dinero

---
+++ src/main/java/com/proyectoColegio/colegio/modelos/Empleado.java
@@ -0,0 +1,68 @@
+package com.proyectoColegio.colegio.modelos;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="Empleado")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    @ManyToOne
+    @JoinColumn(name = "colegio_id")
+    private Colegios colegios;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, Colegios colegios, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.colegios = colegios;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Colegios getColegios() {
+        return colegios;
+    }
+
+    public void setColegios(Colegios colegios) {
+        this.colegios = colegios;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
